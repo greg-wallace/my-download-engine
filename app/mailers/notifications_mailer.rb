28efe8b0df6587112ff381c99e77518b849ebcb4
greg-wallace <greg@icodeforu.com>
2012-08-04 20:20:28 -0700

    back to work

--- app/mailers/notifications_mailer.rb
+++ app/mailers/notifications_mailer.rb
@@ -1,11 +1,11 @@
 class NotificationsMailer < ActionMailer::Base
 
-  default :from => "web@icodeforu.com"
+  default :from => "heroku@icodeforu.com"
   default :to => "greg@icodeforu.com"
 
   def new_message(message)
     @message = message
-    mail(:subject => "[icodeforu.com] #{message.subject}")
+    mail(:subject => "[icodeforu] #{message.subject}")
   end
 
 end
