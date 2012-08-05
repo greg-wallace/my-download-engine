59f50e0460e32510b7dc4b674c8eb57374bda07e
greg-wallace <greg@icodeforu.com>
2012-08-04 20:48:12 -0700

    Revert "back to work"
    This reverts commit 28efe8b0df6587112ff381c99e77518b849ebcb4.

--- config/application.rb
+++ config/application.rb
@@ -41,19 +41,19 @@ module HackerEngine
     # mail contact form.
     
     
-    config.action_mailer.smtp_settings = {
-      :address              => "smtp.gmail.com",
-      :port                 => 587,
-      :domain               => "icodeforu.com",
-      :user_name            => "greg@icodeforu.com",
-      :password             => "OmyKiss135",
-      :authentication       => :plain,
-      :enable_starttls_auto => true
-    }
+     config.action_mailer.smtp_settings = {
+        :address              => "smtp.gmail.com",
+        :port                 => 587,
+        :domain               => "icodeforu.com",
+        :user_name            => "greg@icodeforu.com",
+        :password             => "OmyKiss135",
+        :authentication       => :plain,
+        :enable_starttls_auto => true
+      }
 
-    config.action_mailer.default_url_options = {
-      :host => "icodeforu.com"
-    }
+      config.action_mailer.default_url_options = {
+        :host => "icodeforu.com"
+      }
 
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
